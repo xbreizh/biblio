8fb68da49c369e9a2107e721c6fefce7e1ccd139
xbreizh <xbreizh@yahoo.co.nz>
2019-06-28 17:48:09 +0000

    11- adding tests to mailapp

---
+++ library-mailapp/src/test/java/org/mail/impl/ConnectManagerImplTest.java
@@ -0,0 +1,14 @@
+package org.mail.impl;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+class ConnectManagerImplTest {
+
+
+    @Test
+    void authenticate() {
+    }
+}
---
+++ library-mailapp/src/test/java/org/mail/impl/EmailManagerImplTest.java
@@ -0,0 +1,15 @@
+package org.mail.impl;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+
+@ExtendWith(SpringExtension.class)
+class EmailManagerImplTest {
+
+
+    @Test
+    void sendMail() {
+    }
+}
