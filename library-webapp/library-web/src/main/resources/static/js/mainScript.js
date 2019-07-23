32986134fa479ac88868e16fb5416b55d8e2e7a1
xbreizh <xbreizh@yahoo.co.nz>
2019-07-23 14:41:59 +0000

    #3 added display overdue loans when trying to book

--- library-webapp/library-web/src/main/resources/static/js/mainScript.js
+++ library-webapp/library-web/src/main/resources/static/js/mainScript.js
@@ -17,14 +17,28 @@ function toggle_displays(t, d) {
 }
 
 
-function overdue() {
+function alertOverdue() {
     Swal.fire({
         title: 'You can\'t book while having overdue items!',
         type: 'error',
         confirmButtonText: 'Ok, I ll take care of it!'
     });
+    toggle_displays('myLoans','myProfile' );
 }
 
-
+function overdueInitPopup(overdue) {
+    $(document).ready(function () {
+        if (overdue) {
+            if (sessionStorage.getItem('popState') != 'shown') {
+                Swal.fire({
+                    title: 'You have some overdue items!',
+                    type: 'error',
+                    confirmButtonText: 'Ok, I ll take care of it!'
+                }, document.getElementById("myLoans").style.display = "block");
+                sessionStorage.setItem('popState', 'shown')
+            }
+        }
+    });
+}
 
 
